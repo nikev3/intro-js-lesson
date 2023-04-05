fa0b2b397767e1dcd1f87fd30bf8952d2a9dfc2a
nikev3 <122506274+nikev3@users.noreply.github.com>
2023-04-04 18:07:58 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,24 +1,45 @@
 // 1.
-function sumOfThree(num1, num2, num3) {
-  return num1 + num2 + num3;
+function getPersonStats(name, job, age) {
+  console.log(`Hi my name is ${name}, I am ${age} years old, and I am a ${job}.`);
 }
+
+// Example usage:
+getPersonStats("John", "developer", "30");
+
 
 // 2.
-const productOfThree = function(num1, num2, num3) {
-  return num1 * num2 * num3;
-};
+const sayFirstAndLast = function(str) {
+  const firstChar = str[0];
+  const lastChar = str[str.length - 1];
+  console.log(`the first character is: ${firstChar} and last character is: ${lastChar}`);
+}
+
+// Example usage:
+sayFirstAndLast("hello");
+
 
 // 3.
-const getRemainder = (num1, num2) => {
-  return num1 % num2;
-};
+const yellFruitNum = (num, fruit) => {
+  console.log(`I have ${num} ${fruit.toUpperCase()}!`);
+}
+
+// Example usage:
+yellFruitNum(5, "apples");
+
 
 // 4.
-function getRootRounded(num) {
-  return Math.ceil(Math.sqrt(Math.abs(num)));
+function sliceFruit(fruit) {
+  console.log(fruit.slice(2));
 }
 
+// Example usage:
+sliceFruit("banana");
+
+
 // 5.
-function getRandom() {
-  return Math.floor(Math.random() * 100) + 1;
+function splitFruit(fruit) {
+  return fruit.split("");
 }
+
+// Example usage:
+console.log(splitFruit("orange"));
